011bbc0d205d2aaa0b7cc3c1a64a8f9c2d0ae04e
AleksanderPostrzednik <62178376+AleksanderPostrzednik@users.noreply.github.com>
2025-06-17 23:15:43 +0200

    Add Quarto project for learning methods

---
+++ learning-methods/install.R
@@ -0,0 +1,17 @@
+packages <- c(
+  "tidyverse",
+  "tidytext",
+  "lsa",
+  "topicmodels",
+  "umap",
+  "ggwordcloud",
+  "textmineR"
+)
+
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, repos = "https://cloud.r-project.org")
+  }
+}
+
+invisible(lapply(packages, install_if_missing))
