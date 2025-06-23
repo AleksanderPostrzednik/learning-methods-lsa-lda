1ca09001a53b1edf441e63209418f7067d23eeb9
AleksanderPostrzednik <62178376+AleksanderPostrzednik@users.noreply.github.com>
2025-06-23 17:41:04 +0200

    Update data preprocessing for new CSV

--- scripts/test_data.R
+++ scripts/test_data.R
@@ -1,2 +1,5 @@
-stopifnot(all(c("gender","math_score") %in% 
-              colnames(readr::read_csv("data/student_performance_large_dataset.csv"))))
+cols <- colnames(
+  readr::read_csv("data/student_performance_large_dataset.csv") |>
+    janitor::clean_names()
+)
+stopifnot(all(c("gender", "exam_score_percent") %in% cols))
